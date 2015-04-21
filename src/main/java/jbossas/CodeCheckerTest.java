3616ef5cd5d7ffe8612dc7f01eb68096a0d2e6a0
plummers <plummers@PLUMMERS3.americas.hpqcorp.net>
2015-04-21 07:42:31 -0600

    Has a test

---
+++ src/main/java/jbossas/CodeCheckerTest.java
@@ -0,0 +1,15 @@
+package jbossas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CodeCheckerTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
